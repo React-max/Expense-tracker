58bfb5c1adedefeaa58efbae7311b5d25cebfced
Md. Abid Raihan <abidcsedu@gmail.com>
2023-12-02 17:03:42 +0600

    add data dynamically and filter by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,58 @@
+import { useState } from "react";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
+	const [filteredYear, setFilteredYear] = useState("all");
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	let filteredExpenses;
+	if (filteredYear === "all") {
+		filteredExpenses = props.items;
+	} else {
+		filteredExpenses = props.items.filter(
+			(expense) => expense.date.getFullYear().toString() === filteredYear
+		);
+	}
+
+	let expensesContent = <p>No expenses found on {filteredYear}</p>;
+
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map((expense) => (
+			<ExpenseItem
+				key={expense.id}
+				title={expense.title}
+				amount={expense.amount}
+				date={expense.date}
+			/>
+		));
+	}
+
 	return (
 		<Card className="expenses">
-			<ExpenseItem
+			<ExpenseFilter
+				selected={filteredYear}
+				onChangeFilter={filterChangeHandler}
+			/>
+			{expensesContent}
+			{/* {filteredExpenses.length === 0 ? (
+				<p>No expenses found on {filteredYear}</p>
+			) : (
+				filteredExpenses.map((expense) => (
+					<ExpenseItem
+						key={expense.id}
+						title={expense.title}
+						amount={expense.amount}
+						date={expense.date}
+					/>
+				))
+			)} */}
+			{/* <ExpenseItem
 				title={props.items[0].title}
 				amount={props.items[0].amount}
 				date={props.items[0].date}
@@ -24,7 +71,7 @@ function Expenses(props) {
 				title={props.items[3].title}
 				amount={props.items[3].amount}
 				date={props.items[3].date}
-			/>
+			/> */}
 		</Card>
 	);
 }
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,5 @@
+const ExpensesList = () => {
+	return <div></div>;
+};
+
+export default ExpensesList;
